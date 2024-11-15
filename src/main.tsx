fa614b0248ea4b29fa0ff1bc600c1dc0794171c7
Vapoltavecs <vapoltavecs@gmail.com>
2024-11-15 10:05:25 +0300

    try fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { pathToRegexp } from "path-to-regexp";
+// import { pathToRegexp } from "path-to-regexp";
 
 // console.log(pathToRegexp("/:foo.data.gz"));
 
